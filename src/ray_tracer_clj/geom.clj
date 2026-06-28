c915eaa043591347b757182492fc0f7e90c5e294
Dmitry Zinkevich <mr.zinch@pm.me>
2026-06-28 15:54:42 +0200

    Introduce Tuple data type

---
+++ src/ray_tracer_clj/geom.clj
@@ -0,0 +1,14 @@
+(ns ray-tracer-clj.geom)
+
+(deftype Tuple [^doubles values]
+  clojure.lang.ILookup
+  (valAt [_ k]
+    (case k
+      :x (aget values 0)
+      :y (aget values 1)
+      :z (aget values 2)
+      :w (aget values 3)
+      nil)))
+
+(defn tuple [x y z w]
+  (Tuple. (double-array [x y z w])))
---
+++ test/ray_tracer_clj/geom_test.clj
@@ -0,0 +1,14 @@
+(ns ray-tracer-clj.geom-test
+  (:require [clojure.test :refer :all]
+            [ray-tracer-clj.geom :refer :all]))
+
+(deftest a-tuple-point-test
+  (testing "A tuple with w=1.0 is a point"
+    ;; Given
+    (let [a (tuple 4.3  -4.2  3.1  1.0)]
+
+      ;; Then
+      (is (= 4.3 (:x a)))
+      (is (= -4.2 (:y a)))
+      (is (= 3.1 (:z a)))
+      (is (= 1.0 (:w a))))))
